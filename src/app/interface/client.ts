51f71668c3a5e6ce44cb1c58a95c9a87839b86a9
Anderson Porto <anderson.higo2@gmail.com>
2023-04-04 22:17:17 -0300

    init: basic struct

---
+++ src/app/interface/client.ts
@@ -0,0 +1,9 @@
+export interface IClient {
+  cpf: string;
+  name: string;
+  telephone: string;
+  street: string;
+  number: string;
+  neighborhood: string;
+  salary: number;
+}
---
+++ src/app/service/client.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {IClient} from "../interface/client";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  endpoint = 'clientes';
+  api = environment.api;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllClients() {
+    return this.http.get<IClient[]>(this.api + this.endpoint);
+  }
+
+  getByCpf(cpf: string) {
+    return this.http.get(this.api + this.endpoint + '/' + cpf);
+  }
+
+  createClient(client: IClient) {
+    return this.http.post(this.api + this.endpoint, client);
+  }
+
+  updateClient(client: IClient) {
+    return this.http.put(this.api + this.endpoint, client);
+  }
+
+  deleteClient(cpf: string) {
+    return this.http.delete(this.api + this.endpoint + '/' + cpf);
+  }
+
+}
