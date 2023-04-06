0c395f16d201ee581815aacd1644aba0eacc9f97
Anderson Porto <anderson.higo2@gmail.com>
2023-04-06 01:42:24 -0300

    add: cpf pipe

---
+++ src/app/service/pipe/cpf/cpf.pipe.ts
@@ -0,0 +1,18 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'cpf'})
+export class CpfPipe implements PipeTransform {
+
+  transform(value: string | number): string {
+    let cpfString = value + '';
+
+    cpfString = cpfString.replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1');
+
+    return cpfString;
+  }
+
+}
